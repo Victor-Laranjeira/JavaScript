20cfcdb768aa29c46c2aefd415f4cf0432710e21
Victor Laranjeira <victor-laranjeira18@hotmail.com>
2022-06-22 09:44:07 -0300

    Atualização JavaScript, HTML e CSS

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1037,40 +1037,236 @@ if(age < 18){
 
 // -------------------------------------------------------------------------------------------------
 
-// cookies
+// cookies  21
 
-const firstText = document.querySelector("#firstText");
-const lastText = document.querySelector("#lastText");
-const submitBtn = document.querySelector("#submitBtn");
-const cookieBtn = document.querySelector("#cookieBtn");
+// const firstText = document.querySelector("#firstText");
+// const lastText = document.querySelector("#lastText");
+// const submitBtn = document.querySelector("#submitBtn");
+// const cookieBtn = document.querySelector("#cookieBtn");
 
-submitBtn.addEventListener("click", () => {
-    setCookie("firstName", firstText.value, 365);
-    setCookie("lastName", lastText.value, 365);
-});
-cookieBtn.addEventListener("click", () => {
-    firstText.value = getCookie("firstName");
-    lastText.value = getCookie("lastName");
-});
+// submitBtn.addEventListener("click", () => {
+//     setCookie("firstName", firstText.value, 365);
+//     setCookie("lastName", lastText.value, 365);
+// });
+// cookieBtn.addEventListener("click", () => {
+//     firstText.value = getCookie("firstName");
+//     lastText.value = getCookie("lastName");
+// });
 
-function setCookie(name, value, daysToLive){
-    const date = new Date();
-    date.setTime(date.getTime() +  (daysToLive * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + date.toUTCString();
-    document.cookie = `${name}=${value}; ${expires}; path=/`
-}
-function deleteCookie(name){
-    setCookie(name, null, null);
-}
-function getCookie(name){
-    const cDecoded = decodeURIComponent(document.cookie);
-    const cArray = cDecoded.split("; ");
-    let result = null;
+// function setCookie(name, value, daysToLive){
+//     const date = new Date();
+//     date.setTime(date.getTime() +  (daysToLive * 24 * 60 * 60 * 1000));
+//     let expires = "expires=" + date.toUTCString();
+//     document.cookie = `${name}=${value}; ${expires}; path=/`
+// }
+// function deleteCookie(name){
+//     setCookie(name, null, null);
+// }
+// function getCookie(name){
+//     const cDecoded = decodeURIComponent(document.cookie);
+//     const cArray = cDecoded.split("; ");
+//     let result = null;
     
-    cArray.forEach(element => {
-        if(element.indexOf(name) == 0){
-            result = element.substring(name.length + 1)
-        }
-    })
-    return result;
-}
+//     cArray.forEach(element => {
+//         if(element.indexOf(name) == 0){
+//             result = element.substring(name.length + 1)
+//         }
+//     })
+//     return result;
+// }
+
+// -------------------------------------------------------------------------------------------------
+
+// Stopwatch program   22
+
+// const timeDisplay = document.querySelector("#timeDisplay");
+// const startBtn = document.querySelector("#startBtn");
+// const pauseBtn = document.querySelector("#pauseBtn");
+// const resetBtn = document.querySelector("#resetBtn");
+
+// let startTime = 0;
+// let elapsedTime = 0;
+// let currentTime = 0;
+// let paused = true;
+// let intervalId;
+// let hrs = 0;
+// let mins = 0;
+// let secs = 0;
+
+// startBtn.addEventListener("click", () => {
+//     if(paused){
+//         paused = false;
+//         startTime = Date.now() - elapsedTime;
+//         intervalId = setInterval(updateTime, 1000);
+//     }
+// });
+// pauseBtn.addEventListener("click", () => {
+//     if(!paused){
+//         paused = true;
+//         elapsedTime = Date.now() - startTime;
+//         clearInterval(intervalId);
+//     }
+// });
+// resetBtn.addEventListener("click", () => {
+//     paused = true;
+//     clearInterval(intervalId);
+//     startTime = 0;
+//     elapsedTime = 0;
+//     currentTime = 0;
+//     hrs = 0;
+//     mins = 0;
+//     secs = 0;
+//     timeDisplay.textContent = "00:00:00";
+// });
+
+// function updateTime(){
+//     elapsedTime = Date.now() - startTime;
+
+//     secs = Math.floor((elapsedTime / 1000) % 60);
+//     mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+//     hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+
+//     secs = pad(secs);
+//     mins = pad(mins);
+//     mins = pad(mins);
+
+//     timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
+//     function pad(unit){
+//         return (("0") + unit).length > 2 ? unit : "0" + unit;
+//     }
+// }
+
+// --------------------------------------------------------------------------------------------------
+
+// Rock Paper Scissors game  23
+
+// const playerText = document.querySelector("#playerText");
+// const computerText = document.querySelector("#computerText");
+// const resultText = document.querySelector("#resultText");
+// const choiceBtns = document.querySelectorAll(".choiceBtn");
+
+// let player;
+// let computer;
+// let result;
+
+// choiceBtns.forEach(button => button.addEventListener("click", () => {
+//     player = button.textContent;
+//     computerTurn();
+//     playerText.textContent = `Player: ${player}`;
+//     computerText.textContent = `Computer: ${computer}`;
+//     resultText.textContent = checkWinner();
+// }));
+
+// function computerTurn(){
+//     const randNum = Math.floor(Math.random() * 3) + 1;
+
+//     switch(randNum){
+//         case 1:
+//             computer = "Rock";
+//             break;
+//         case 2:
+//             computer = "Paper";
+//             break;
+//         case 3:
+//             computer = "Scissors";
+//             break;
+//     }
+// }
+
+// function checkWinner(){
+//     if(player == computer){
+//         return "Draw";
+//     }else if(computer == "Rock"){
+//         return (player == "Paper") ? "You win" : "You lose";
+//     }else if(computer == "Paper"){
+//         return (player == "Scissors") ? "You win" : "You lose";
+//     }else if(computer == "Scissors"){
+//         return (player == "Rock") ? "You win" : "You lose";
+//     }
+// }
+
+// -------------------------------------------------------------------------------------------------
+
+// Tic-Tac-Toe  24
+
+// const cells = document.querySelectorAll(".cell");
+// const statusText = document.querySelector("#statusText");
+// const restartBtn = document.querySelector("#restartBtn");
+// const winConditions = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6]
+// ];
+// let options = ["", "", "", "", "", "", "", "", ""];
+// let currentPlayer = "X";
+// let running = false;
+
+// initializeGame();
+
+// function initializeGame(){
+//     cells.forEach(cell => cell.addEventListener("click", cellClicked));
+//     restartBtn.addEventListener("click", restartGame);
+//     statusText.textContent = `${currentPlayer}'s turn`;
+//     running = true;
+// }
+// function cellClicked(){
+//     const cellIndex = this.getAttribute("cellIndex");
+
+//     if(options[cellIndex] != "" || !running){
+//         return;
+//     }
+
+//     updateCell(this, cellIndex);
+//     checkWinner();
+// }
+// function updateCell(cell, index){
+//     options[index] = currentPlayer;
+//     cell.textContent = currentPlayer;
+// }
+// function changePlayer(){
+//     currentPlayer = (currentPlayer == "X") ? "O" : "X";
+//     statusText.textContent = `${currentPlayer}'s turn`;
+// }
+// function checkWinner(){
+//     let roundWon = false;
+
+//     for(let i = 0; i < winConditions.length; i++){
+//         const condition = winConditions[i];
+//         const cellA = options[condition[0]];
+//         const cellB = options[condition[1]];
+//         const cellC = options[condition[2]];
+
+//         if(cellA == "" || cellB == "" || cellC == ""){
+//             continue;
+//         }
+//         if(cellA == cellB && cellB == cellC){
+//             roundWon = true;
+//             break;
+//         }
+//     }
+
+//     if(roundWon){
+//         statusText.textContent = `${currentPlayer} wins!`;
+//         running = false;
+//     }
+//     else if(!options.includes("")){
+//         statusText.textContent = `Draw!`;
+//         running = false;
+//     }
+//     else{
+//         changePlayer();
+//     }
+// }
+// function restartGame(){
+//     currentPlayer = "X";
+//     options = ["", "", "", "", "", "", "", "", ""];
+//     statusText.textContent = `${currentPlayer}'s turn`;
+//     cells.forEach(cell => cell.textContent = "");
+//     running = true;
+// }
